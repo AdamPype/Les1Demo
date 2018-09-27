17d39a14d0bf68cfe46ba3c5a6856ffdefe13b37
AdamPype <adam.pype@student.howest.be>
2018-09-27 14:44:39 +0200

    Jumping and drag

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -10,14 +10,16 @@ public class PlayerScript : MonoBehaviour {
     
     [SerializeField] private float _acceleration;
     [SerializeField] private float _drag;
+    [SerializeField] private float _maximumXZVelocity = (30 * 1000) / (60 * 60); //[m/s] 30km/h
+    [SerializeField] private float _jumpHeight;
 
     private Vector3 _velocity = Vector3.zero; // [m/s]
     private Vector3 _inputMovement;
+    private bool _jump;
 
     private Transform _absoluteTransform;
     private CharacterController _char;
     private MeshRenderer _rend;
-
 
     void Start ()
         {
@@ -33,19 +35,46 @@ public class PlayerScript : MonoBehaviour {
 
         }
 
+    private void Update()
+        {
+        _inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));  //.normalized;
+        if (Input.GetButtonDown("Jump"))
+            {
+            _jump = true;
+            }
+
+        DoRainbow();
+        }
+
     void FixedUpdate ()
         {
         ApplyGround();
         ApplyGravity();
         ApplyMovement();
         ApplyDragOnGround();
+        ApplyJump();
+        LimitXZVelocity();
 
         DoMovement();
         }
 
-    private void Update()
+
+    private void ApplyGround()
         {
-        _inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));  //.normalized;
+        if (_char.isGrounded)
+            {
+            //ground velocity
+            _velocity -= Vector3.Project(_velocity, Physics.gravity);
+            }
+        }
+
+    private void ApplyGravity()
+        {
+        if (!_char.isGrounded)
+            {
+            //apply gravity
+            _velocity += Physics.gravity * Time.deltaTime;
+            }
         }
 
     private void ApplyMovement()
@@ -59,9 +88,18 @@ public class PlayerScript : MonoBehaviour {
             //move in relative direction
             Vector3 relativeMov = relativeRot * _inputMovement;
             _velocity += relativeMov * _acceleration * Time.deltaTime;
-
             }
 
+        }
+
+    private void LimitXZVelocity()
+        {
+        Vector3 yVel = Vector3.Scale(_velocity, Vector3.up);
+        Vector3 xzVel = Vector3.Scale(_velocity, new Vector3(1, 0, 1));
+
+        xzVel = Vector3.ClampMagnitude(xzVel, _maximumXZVelocity);
+
+        _velocity = xzVel + yVel;
         }
 
     private void ApplyDragOnGround()
@@ -73,6 +111,15 @@ public class PlayerScript : MonoBehaviour {
             }
         }
 
+    private void ApplyJump()
+        {
+        if (_char.isGrounded && _jump)
+            {
+            _velocity.y += Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
+            _jump = false;
+            }
+        }
+
     private void DoMovement()
         {
         //do velocity / movement on character controller
@@ -80,21 +127,17 @@ public class PlayerScript : MonoBehaviour {
         _char.Move(movement);
         }
 
-    private void ApplyGravity()
+    private void DoRainbow()
         {
-        if (!_char.isGrounded)
+        float h;
+        float s;
+        float v;
+        Color.RGBToHSV(_rend.material.color, out h, out s, out v);
+        h += Time.deltaTime;
+        if (h > 1)
             {
-            //apply gravity
-            _velocity += Physics.gravity * Time.deltaTime;
+            h = 0;
             }
-        }
-
-    private void ApplyGround()
-        {
-        if (_char.isGrounded)
-            {
-            //ground velocity
-            _velocity -= Vector3.Project(_velocity, Physics.gravity);
-            }
+        _rend.material.color = Color.HSVToRGB(h, 0.8f, 1);
         }
     }
