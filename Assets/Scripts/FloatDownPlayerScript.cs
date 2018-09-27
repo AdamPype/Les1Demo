bd57606bacea2c4009b5264237c33799d8150dff
AdamPype <adam.pype@student.howest.be>
2018-09-27 18:51:02 +0200

    Made exercise
    Added 4 new scripts to the player. Enable one and disable the other to test it out!

--- Assets/Scripts/FloatDownPlayerScript.cs
+++ Assets/Scripts/FloatDownPlayerScript.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 //[RequireComponent(typeof(CharacterController))]
-public class PlayerScript : MonoBehaviour {
+public class FloatDownPlayerScript : MonoBehaviour {
 
     
     [SerializeField] private float _acceleration;
     [SerializeField] private float _drag;
     [SerializeField] private float _maximumXZVelocity = (30 * 1000) / (60 * 60); //[m/s] 30km/h
     [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _floatSpeed;
 
     private Vector3 _velocity = Vector3.zero; // [m/s]
     private Vector3 _inputMovement;
     private bool _jump;
+    private float _nonGroundedTimer = 0;
+    private bool _isJumping;
 
     private Transform _absoluteTransform;
     private CharacterController _char;
@@ -38,7 +41,7 @@ public class PlayerScript : MonoBehaviour {
     private void Update()
         {
         _inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));  //.normalized;
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && _nonGroundedTimer < 3)
             {
             _jump = true;
             }
@@ -48,6 +51,7 @@ public class PlayerScript : MonoBehaviour {
 
     void FixedUpdate ()
         {
+        CalculateStableGrounded();
         ApplyGround();
         ApplyGravity();
         ApplyMovement();
@@ -58,6 +62,19 @@ public class PlayerScript : MonoBehaviour {
         DoMovement();
         }
 
+    private void CalculateStableGrounded()
+        {
+        //make sure grounded is correct
+        if (!_char.isGrounded)
+            {
+            _nonGroundedTimer++;
+            }
+        else
+            {
+            _nonGroundedTimer = 0;
+            _isJumping = false;
+            }
+        }
 
     private void ApplyGround()
         {
@@ -72,14 +89,22 @@ public class PlayerScript : MonoBehaviour {
         {
         if (!_char.isGrounded)
             {
-            //apply gravity
-            _velocity += Physics.gravity * Time.deltaTime;
+            if (_velocity.y < 0 && _isJumping)
+                {
+                //apply low gravity
+                _velocity.y = -_floatSpeed;
+                }
+            else
+                {
+                //apply gravity
+                _velocity += Physics.gravity * Time.deltaTime;
+                }
             }
         }
 
     private void ApplyMovement()
         {
-        if (_char.isGrounded)
+        if (_char.isGrounded || (_isJumping && _velocity.y < 0))
             {
             //get relative rotation from camera
             Vector3 xzForward = Vector3.Scale(_absoluteTransform.forward, new Vector3(1, 0, 1));
@@ -104,19 +129,22 @@ public class PlayerScript : MonoBehaviour {
 
     private void ApplyDragOnGround()
         {
-        if (_char.isGrounded)
+        if (_char.isGrounded || (_isJumping && _velocity.y < 0))
             {
+            float saveY = _velocity.y;
             //drag
             _velocity = _velocity * (1 - _drag * Time.deltaTime); //same as lerp
+            _velocity.y = saveY;
             }
         }
 
     private void ApplyJump()
         {
-        if (_char.isGrounded && _jump)
+        if (_nonGroundedTimer < 3 && _jump)
             {
             _velocity.y += Mathf.Sqrt(2 * Physics.gravity.magnitude * _jumpHeight);
             _jump = false;
+            _isJumping = true;
             }
         }
 
